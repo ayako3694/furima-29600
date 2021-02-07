83c86076f08ef894651029e2571d624aae1f8ec7
ayako3694 <ayako3694@gmail.com>
2021-02-07 22:07:53 +0900

    ユーザー管理機能の実装①新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+# バリデーションの設定(空の文字列を保存させない為と一意性制約)
+validates :nick_name,               presence: true
+validates :encrypted_password,     presence: true
+validates :last_name,              presence: true
+validates :first_name,             presence: true
+validates :last_name_kana,         presence: true
+validates :first_name_kana,        presence: true
+validates :birth_date,             presence: true
+
+# アソシエーション
+has_many :items
+has_many :buyers
 end
